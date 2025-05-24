a6ca351e67914c012e50d73640a4e7035f7b7911
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-05-24 16:10:19 +0530

    initial commit

---
+++ src/main/java/com/springboot_interceptor/SpringbootInterceptorApplication.java
@@ -0,0 +1,14 @@
+package com.springboot_interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterceptorApplication.class, args);
+		System.out.println("Running......");
+	}
+
+}
